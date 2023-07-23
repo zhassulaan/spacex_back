9e9833510be0577b806e5ca93566872f914ebba4
Zhassulan <190103383@stu.sdu.edu.kz>
2023-07-23 13:16:40 +0600

    d

--- back/settings.py
+++ back/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+     'corsheaders',
     # apps
     'home',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,7 +73,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'back.wsgi.application'
-
+CORS_ORIGIN_ALLOW_ALL = True  # Allow requests from any origin
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
